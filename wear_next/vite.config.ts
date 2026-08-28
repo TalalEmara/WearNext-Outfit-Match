afa24bbfc2383a783ce2344eafc29a856388e7ad
TalalEmara <126427309+TalalEmara@users.noreply.github.com>
2026-08-28 16:27:42 +0300

    deploy setup add nitro

--- wear_next/vite.config.ts
+++ wear_next/vite.config.ts
@@ -1,13 +1,14 @@
 import { defineConfig } from 'vite'
 import { devtools } from '@tanstack/devtools-vite'
 import { tanstackStart } from '@tanstack/react-start/plugin/vite'
-import netlify from '@netlify/vite-plugin-tanstack-start'
+import { nitro } from 'nitro/vite'
 import viteReact from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 const config = defineConfig({
   resolve: { tsconfigPaths: true },
-  plugins: [devtools(), tailwindcss(), tanstackStart(), viteReact(), netlify()],
+  plugins: [devtools(), tailwindcss(), tanstackStart(), nitro(), viteReact()],
 })
 
 export default config
+
